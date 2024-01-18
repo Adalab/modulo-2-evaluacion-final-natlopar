8564cee92cae9e2af08f96150bf1d40c6184953d
natlopar <natalialopezariel@gmail.com>
2024-01-18 21:03:07 +0100

    html and css

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,7 @@
 'use strict';
+
+const input = document.querySelector('.js-input');
+const btnSearch = document.querySelector('.js-btnSearch');
+const containerFav = document.querySelector('.js-containerFav');
+const containerRes = document.querySelector('.js-containerRes');
+
