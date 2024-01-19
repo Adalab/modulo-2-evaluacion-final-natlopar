1538f0085406cf7d3235eadcb36968f02be17057
natlopar <natalialopezariel@gmail.com>
2024-01-19 12:11:52 +0100

    favorit list

--- src/js/main.js
+++ src/js/main.js
@@ -22,6 +22,7 @@ function handleSearchApi(event) {
 btnSearch.addEventListener('click', handleSearchApi);
 
 function renderSeries(arraySeries, container) {
+container.innerHTML= '';  //esto tengo que hacerlo para que cuando llame a renderizar me vacíe el contenido primero, si no, se me acumulaban todas las búsquedas y las favoritas al llamar a render
   for (const serie of arraySeries) {
     const idCard = serie.mal_id;
     // console.log(serie);
@@ -30,8 +31,7 @@ function renderSeries(arraySeries, container) {
     let imageUrl = serie.images.jpg.image_url;
     const newImageUrl = 'https://placehold.co/210x295?text=Image';
     if (
-      imageUrl ===
-      'https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png'
+      imageUrl ==='https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png'
     ) {
       imageUrl = newImageUrl;
     }
@@ -65,14 +65,21 @@ function handleAddFavorites(event) {
   card.classList.add('favorite');
   const idCardSelected = card.id;
   const foundSerieId = seriesResult.find((oneSerie) => 
-    parseInt(idCardSelected) === oneSerie.mal_id
-    );
+    parseInt(idCardSelected) === oneSerie.mal_id);
    console.log(foundSerieId);//POR FIN CONSIGO EL OBJETO CLICKADO!!
-    favoriteSeries.push(foundSerieId);
+    const indexFav = favoriteSeries.findIndex((oneSerie) => oneSerie.mal_id === parseInt(idCardSelected));
+    if(indexFav === -1){
+        favoriteSeries.push(foundSerieId);
+    }
     console.log(favoriteSeries);
-
-    ///si ya está en el array, no añadirlo, o sea antes del push
     renderSeries(favoriteSeries, containerFav);
+    localStorage.setItem('series', JSON.stringify(favoriteSeries));
+}
+  
+   
+function getLocalSeries () {
+    const seriesLocalStorage = JSON.parse(localStorage.getItem('series'));
+    renderSeries(seriesLocalStorage, containerFav);
 }
 
-
+getLocalSeries();
