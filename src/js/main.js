b244d29a79f9b305811b0d4eb854fe0f6dec97d0
natlopar <natalialopezariel@gmail.com>
2024-01-19 15:44:39 +0100

    favorites

--- src/js/main.js
+++ src/js/main.js
@@ -1,32 +1,33 @@
 'use strict';
 
+
+
 const input = document.querySelector('.js-input');
 const btnSearch = document.querySelector('.js-btnSearch');
 const containerFav = document.querySelector('.js-containerFav');
 const containerRes = document.querySelector('.js-containerRes');
+
+
 let seriesResult = []; // array.data, contiene los arrays
 let favoriteSeries = [];
 
 function handleSearchApi(event) {
   event.preventDefault();
   const inputValue = input.value.toLowerCase();
-  console.log(inputValue);
   fetch(`https://api.jikan.moe/v4/anime?q=${inputValue}`)
     .then((response) => response.json())
     .then((dataApi) => {
       seriesResult = dataApi.data;
       renderSeries(seriesResult, containerRes);
     });
+    
 }
 
 btnSearch.addEventListener('click', handleSearchApi);
 
 function renderSeries(arraySeries, container) {
-container.innerHTML= '';  //esto tengo que hacerlo para que cuando llame a renderizar me vacíe el contenido primero, si no, se me acumulaban todas las búsquedas y las favoritas al llamar a render
+container.innerHTML= '';  
   for (const serie of arraySeries) {
-    const idCard = serie.mal_id;
-    // console.log(serie);
-    // console.log(idCard);
     const titleText = document.createTextNode(serie.title);
     let imageUrl = serie.images.jpg.image_url;
     const newImageUrl = 'https://placehold.co/210x295?text=Image';
@@ -42,50 +43,83 @@ container.innerHTML= '';  //esto tengo que hacerlo para que cuando llame a rende
     const icon = document.createElement('i');
     img.setAttribute('src', imageUrl);
     img.setAttribute('class', 'card__img');
-    // img.setAttribute('alt', 'imagen anime');
+    img.setAttribute('alt', 'imagen anime');
     container.appendChild(li);
     li.appendChild(articleEl);
+    articleEl.appendChild(icon);
     articleEl.appendChild(img);
     title.appendChild(titleText);
     icon.setAttribute('class', 'fa-solid fa-circle-xmark js-btnRemove card__btn' );
+    icon.setAttribute('id', serie.mal_id);
     articleEl.setAttribute('class', 'js-article card');
-    articleEl.setAttribute('id', idCard);
+    articleEl.setAttribute('id', serie.mal_id);
     articleEl.appendChild(title);
-    if (container === containerFav) {
-        articleEl.appendChild(icon);
+    if (container === containerRes) {
+        articleEl.removeChild(icon);
     }
     container.appendChild(li);
-    listenerSeries();
+    selectFavorites();
+
+
+    const btnRemove = document.querySelectorAll('.js-btnRemove');
+    for(const button of btnRemove){
+        button.addEventListener('click', handleRemoveFavorite);
+    }
+ 
+
   }
 }
 
-function listenerSeries() {
+function selectFavorites() {
   const articles = document.querySelectorAll('.js-article');
   for (const article of articles) {
-    article.addEventListener('click', handleAddFavorites);
+    article.addEventListener('click', handleAddFavorite);
   }
 }
 
-function handleAddFavorites(event) {
+function handleAddFavorite(event) {
   const card = event.currentTarget;
   card.classList.add('favorite');
   const idCardSelected = card.id;
   const foundSerieId = seriesResult.find((oneSerie) => 
     parseInt(idCardSelected) === oneSerie.mal_id);
-   console.log(foundSerieId);//POR FIN CONSIGO EL OBJETO CLICKADO!!
+//    console.log(foundSerieId);//POR FIN CONSIGO EL OBJETO CLICKADO!!
     const indexFav = favoriteSeries.findIndex((oneSerie) => oneSerie.mal_id === parseInt(idCardSelected));
     if(indexFav === -1){
         favoriteSeries.push(foundSerieId);
     }
-    console.log(favoriteSeries);
+    // console.log(favoriteSeries);
     renderSeries(favoriteSeries, containerFav);
     localStorage.setItem('series', JSON.stringify(favoriteSeries));
 }
   
    
 function getLocalSeries () {
+    
     const seriesLocalStorage = JSON.parse(localStorage.getItem('series'));
+    if (seriesLocalStorage) {
     renderSeries(seriesLocalStorage, containerFav);
+    } 
 }
 
 getLocalSeries();
+
+let newFavoriteSeries= [];
+function handleRemoveFavorite (event) {
+    const btnClick = event.currentTarget;
+    console.log(btnClick.id);
+//id de la tarjeta q quiero borrar, pero cada vez q hago click cambia!
+    const foundSerieId = seriesResult.find((oneSerie) => 
+    parseInt(btnClick.id) === oneSerie.mal_id);
+    console.log(foundSerieId.mal_id);
+    let indexRemove = favoriteSeries.findIndex((serie) => serie.mal_id === foundSerieId.mal_id);
+    console.log(indexRemove);
+  
+    favoriteSeries.splice(indexRemove, 1);
+    renderSeries(favoriteSeries,containerFav);
+    console.log(favoriteSeries);
+   //no consigo actualizar array ni borrar
+    // localStorage.setItem('series', JSON.stringify(favoriteSeries));
+    
+}
+
