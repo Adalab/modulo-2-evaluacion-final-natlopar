e0241ca55c2546ceed43be7c808fa98cd414c13f
natlopar <natalialopezariel@gmail.com>
2024-01-19 18:45:48 +0100

    changing favorites styles

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,9 @@
 'use strict';
 
-
-
 const input = document.querySelector('.js-input');
 const btnSearch = document.querySelector('.js-btnSearch');
 const containerFav = document.querySelector('.js-containerFav');
 const containerRes = document.querySelector('.js-containerRes');
-
 
 let seriesResult = []; // array.data, contiene los arrays
 let favoriteSeries = [];
@@ -20,19 +17,19 @@ function handleSearchApi(event) {
       seriesResult = dataApi.data;
       renderSeries(seriesResult, containerRes);
     });
-    
 }
 
 btnSearch.addEventListener('click', handleSearchApi);
 
 function renderSeries(arraySeries, container) {
-container.innerHTML= '';  
+  container.innerHTML = '';
   for (const serie of arraySeries) {
     const titleText = document.createTextNode(serie.title);
     let imageUrl = serie.images.jpg.image_url;
-    const newImageUrl = 'https://placehold.co/210x295?text=Image';
+    const newImageUrl = 'https://placehold.co/210x295?text=No\Image\Found';
     if (
-      imageUrl ==='https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png'
+      imageUrl ===
+      'https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png'
     ) {
       imageUrl = newImageUrl;
     }
@@ -49,28 +46,32 @@ container.innerHTML= '';
     articleEl.appendChild(icon);
     articleEl.appendChild(img);
     title.appendChild(titleText);
-    icon.setAttribute('class', 'fa-solid fa-circle-xmark js-btnRemove card__btn' );
+    icon.setAttribute(
+      'class',
+      'fa-solid fa-circle-xmark js-btnRemove card__btn'
+    );
     icon.setAttribute('id', serie.mal_id);
     articleEl.setAttribute('class', 'js-article card');
     articleEl.setAttribute('id', serie.mal_id);
     articleEl.appendChild(title);
     if (container === containerRes) {
-        articleEl.removeChild(icon);
+      li.setAttribute('class', 'js-liResult');
+      articleEl.removeChild(icon);
     }
     container.appendChild(li);
-    selectFavorites();
+    listenerSelectFavorites();
+    listenerRemoveFavorites();
+  }
 
-
+  function listenerRemoveFavorites() {
     const btnRemove = document.querySelectorAll('.js-btnRemove');
-    for(const button of btnRemove){
-        button.addEventListener('click', handleRemoveFavorite);
+    for (const button of btnRemove) {
+      button.addEventListener('click', handleRemoveFavorite);
     }
- 
-
   }
 }
 
-function selectFavorites() {
+function listenerSelectFavorites() {
   const articles = document.querySelectorAll('.js-article');
   for (const article of articles) {
     article.addEventListener('click', handleAddFavorite);
@@ -79,47 +80,62 @@ function selectFavorites() {
 
 function handleAddFavorite(event) {
   const card = event.currentTarget;
+  const liResult = card.parentElement;
   card.classList.add('favorite');
-  const idCardSelected = card.id;
-  const foundSerieId = seriesResult.find((oneSerie) => 
-    parseInt(idCardSelected) === oneSerie.mal_id);
-//    console.log(foundSerieId);//POR FIN CONSIGO EL OBJETO CLICKADO!!
-    const indexFav = favoriteSeries.findIndex((oneSerie) => oneSerie.mal_id === parseInt(idCardSelected));
-    if(indexFav === -1){
-        favoriteSeries.push(foundSerieId);
+  console.log(liResult);
+  if (liResult.classList.contains('js-liResult')) {
+    //quiero que solo lo haga en las fotos de resultados, no en favoritos
+    
+    const idCardSelected = card.id;
+    const foundSerieId = seriesResult.find(
+      (oneSerie) => parseInt(idCardSelected) === oneSerie.mal_id
+    );
+    //    console.log(foundSerieId);//POR FIN CONSIGO EL OBJETO CLICKADO!!
+    const indexFav = favoriteSeries.findIndex(
+      (oneSerie) => oneSerie.mal_id === parseInt(idCardSelected)
+    );
+    if (indexFav === -1) {
+      favoriteSeries.push(foundSerieId);
     }
-    // console.log(favoriteSeries);
     renderSeries(favoriteSeries, containerFav);
     localStorage.setItem('series', JSON.stringify(favoriteSeries));
+  }
 }
-  
-   
-function getLocalSeries () {
-    
-    const seriesLocalStorage = JSON.parse(localStorage.getItem('series'));
-    if (seriesLocalStorage) {
+
+function getLocalSeries() {
+  const seriesLocalStorage = JSON.parse(localStorage.getItem('series'));
+  if (seriesLocalStorage) {
     renderSeries(seriesLocalStorage, containerFav);
-    } 
+  }
 }
 
 getLocalSeries();
 
-let newFavoriteSeries= [];
-function handleRemoveFavorite (event) {
-    const btnClick = event.currentTarget;
-    console.log(btnClick.id);
-//id de la tarjeta q quiero borrar, pero cada vez q hago click cambia!
-    const foundSerieId = seriesResult.find((oneSerie) => 
-    parseInt(btnClick.id) === oneSerie.mal_id);
-    console.log(foundSerieId.mal_id);
-    let indexRemove = favoriteSeries.findIndex((serie) => serie.mal_id === foundSerieId.mal_id);
-    console.log(indexRemove);
+function handleRemoveFavorite(event) {
+  const btnClick = event.currentTarget;
+  const articleClick = btnClick.parentElement;
+  console.log(articleClick);
+  const idFavoriteClick = btnClick.id;
+  console.log(parseInt(idFavoriteClick));
+  const indexRemove = favoriteSeries.findIndex(
+    (serie) => serie.mal_id === parseInt(idFavoriteClick)
+  );
+  favoriteSeries.splice(indexRemove, 1);
+  localStorage.setItem('series',JSON.stringify(favoriteSeries));
+  renderSeries(favoriteSeries, containerFav);
+
+  //qUITAR LA CLASE EN RESULTS
+  // const indexResult = seriesResult.findIndex(
+  //   (serie) => serie.mal_id === parseInt(idFavoriteClick));
+  // for(const serie of seriesResult){
+  //   if(serie.mal_id === parseInt(idFavoriteClick)) {
+  //     articleClick.classList.remove('favorite');
+  //   }
+
+  }
   
-    favoriteSeries.splice(indexRemove, 1);
-    renderSeries(favoriteSeries,containerFav);
-    console.log(favoriteSeries);
-   //no consigo actualizar array ni borrar
-    // localStorage.setItem('series', JSON.stringify(favoriteSeries));
-    
-}
+  // console.log(newResultsSeries);
+  
+    // card.classList.add('favorite');
+
 
